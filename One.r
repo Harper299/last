8a86ef82dc28dc8750e00090a387a66233974164
Harper299 <masschandru25@gmail.com>
2026-03-14 20:20:10 +0530

    Add R script demonstrating data types and structures
    This file contains various data types and structures in R, including numeric, integer, character, logical, vector, sequence, repetition, matrix, list, and data frame examples.

---
+++ One.r
@@ -0,0 +1,35 @@
+# Numeric data
+num <- 25.5
+# Integer data
+int <- 10L
+# Character data
+char <- "R Programming"
+# Logical data
+logic <- TRUE
+# Vector
+vec <- c(10, 20, 30)
+# Sequence
+seq_data <- seq(1, 10, by = 2)
+# Repetition
+rep_data <- rep(5, times = 4)
+# Matrix
+mat <- matrix(1:6, nrow = 2, ncol = 3)
+# List
+lst <- list(1, "Data", FALSE)
+# Data Frame
+df <- data.frame(
+ ID = c(1, 2),
+  Name = c("Amit", "Rita"),
+ Marks = c(85, 90)
+)
+# Display output
+print(num)
+print(int)
+print(char)
+print(logic)
+print(vec)
+print(seq_data)
+print(rep_data)
+print(mat)
+print(lst)
+print(df)
